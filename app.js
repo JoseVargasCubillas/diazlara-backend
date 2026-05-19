7fbe95e4572932e08db7cb742d09949d4a00ff2f
DevMauricioCR <211106@ids.upchiapas.edu.mx>
2026-05-19 11:01:15 -0600

    Add Hostinger Node entrypoint

---
+++ app.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+const entrypoint = path.join(__dirname, 'dist', 'index.js');
+
+if (!fs.existsSync(entrypoint)) {
+  console.error('No se encontro dist/index.js. Ejecuta "npm run build" antes de iniciar la app.');
+  process.exit(1);
+}
+
+require(entrypoint);
